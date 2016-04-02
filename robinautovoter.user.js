38cfb1a9a2cdab544b84aaa7fe1db4bfa898c8a1
tmas <tmas@tgaragemedia.com>
2016-04-01 21:20:26 -0400

    Update robinautovoter.user.js

--- robinautovoter.user.js
+++ robinautovoter.user.js
@@ -9,6 +9,8 @@
 // ==/UserScript==
 /* jshint -W097 */
 'use strict';
+var use_stay_cutoff = true; // Should the bot start voting stay after the room reaches stay_cutoff participants?
+var stay_cutoff = 100; // The number of participants at which the bot should start voting stay
 
 function sendMessage(message){
     $("#robinSendMessage > input[type='text']").val(message);
@@ -21,9 +23,16 @@ setTimeout(function(){
     if (participants == 200) partiText = 200 + " " + $(".robin-user-list-overflow-indicator").text();
     else partiText = participants;
     
-    sendMessage("/vote grow");
-    if(Math.random() < 0.2) sendMessage("[Robin Autovoter 1.9] Autovoted grow! https://www.reddit.com/r/joinrobin/comments/4cx02w/better_working_automatic_grow_script/");
-    setTimeout(function(){sendMessage("[Robin Autovoter 1.9] " + partiText + " in this room! " + $("span:contains('Voting will end')").first().text());}, 10000);
+    var advChance = 0.8 / participants;
+    
+    if (!use_stay_cutoff || participants < stay_cutoff) {
+        sendMessage("/vote grow");
+        if(Math.random() < advChance) sendMessage("[Robin Autovoter 1.9-mod] Autovoted grow! https://www.reddit.com/r/joinrobin/comments/4cx02w/better_working_automatic_grow_script/d1mhlzo");
+    } else {
+        sendMessage("/vote stay");
+        if(Math.random() < advChance) sendMessage("[Robin Autovoter 1.9-mod] Autovoted stay because participant count >= " + stay_cutoff + ". https://www.reddit.com/r/joinrobin/comments/4cx02w/better_working_automatic_grow_script/d1mhlzo");
+    }
+    setTimeout(function(){if(Math.random() < (4.0 / participants)) sendMessage("[Robin Autovoter 1.9-mod] " + partiText + " in this room! " + $("span:contains('Voting will end')").first().text());}, 10000);
     setTimeout(function(){
         window.location.reload();
     }, 300000);
